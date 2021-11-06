f64c99a88e6de2caafbe08d6faab7dfcafc9fd82
Will Campbell <wibb1@comcast.net>
2021-11-06 13:49:12 -0400

    Connected existing components to store

--- Section19-AdvancedRedux/src/store/cart-slice.js
+++ Section19-AdvancedRedux/src/store/cart-slice.js
@@ -1,31 +1,42 @@
-import { useSelector, useDispatch } from "react-redux";
 import { createSlice } from "@reduxjs/toolkit";
 
-const cartSlice =()=>{
-  createSlice({name:cartSlice, initialState: {
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
     items: [],
     totalQuantity: 0,
   },
   reducers: {
-    addItem(state, action){
-      const newItem = action.payload
-      const existingItem = state.items.find(item => item.id === newItem.id)
-      if(!existingItem){
-        state.items.push({itemId: newItem.id, price: newItem.price, quantity: 1, totalPrice: newItem.price, name: newItem.title})
+    addItem(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
+      if (!existingItem) {
+        state.items.push({
+          id: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title,
+        });
       } else {
-        existingItem.quantity++,
-        existingItem.totalPrice = existingItem.price * existingItem.quantity,
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.price * existingItem.quantity;
       }
-
     },
-    removeItem(state, action){
-      const removedItem = action.payload
-      const exitingItem = state.items.find(item => item.id === removedItem.id)
+    removeItem(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+      }
     },
+  },
+});
 
-  }
-})
-  
-}
+export const cartActions = cartSlice.actions;
 
-export default cartSlice
+export default cartSlice;
