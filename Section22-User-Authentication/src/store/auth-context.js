20b36bdc261dc3bf3cf03c310b4975917989b43d
Will Campbell <wibb1@comcast.net>
2021-12-18 11:32:43 -0500

    store token in local storage

--- Section22-User-Authentication/src/store/auth-context.js
+++ Section22-User-Authentication/src/store/auth-context.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 const AuthContext = React.createContext({
   token: "",
@@ -8,16 +8,19 @@ const AuthContext = React.createContext({
 });
 
 export const AuthContextProvider = (props) => {
-  const [token, setToken] = useState(null);
+  const initialToken = localStorage.getItem('token')
+  const [token, setToken] = useState(initialToken);
 
   const userLoggedIn = !!token;
 
   const loginHandler = (token) => {
     setToken(token);
+    localStorage.setItem('token', token);
   };
 
   const logoutHandler = () => {
-    setToken(null);
+    setToken(undefined);
+    localStorage.removeItem('token')
   };
 
   const contextValue = {
@@ -33,4 +36,4 @@ export const AuthContextProvider = (props) => {
   );
 };
 
-export default AuthContext
+export default AuthContext;
