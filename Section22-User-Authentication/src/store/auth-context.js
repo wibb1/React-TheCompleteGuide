4c20e7d7cb32688da2cbbb6f18cdefe18177f068
Will Campbell <wibb1@comcast.net>
2021-12-18 11:46:55 -0500

    added logout timer

--- Section22-User-Authentication/src/store/auth-context.js
+++ Section22-User-Authentication/src/store/auth-context.js
@@ -7,21 +7,30 @@ const AuthContext = React.createContext({
   logout: () => {},
 });
 
+const calculateRemainingTime = (expirationTime)=>{
+  const currentTime = new Date().getTime();
+  const newExpirationTime = new Date(expirationTime).getTime()
+  return newExpirationTime - currentTime
+}
+
 export const AuthContextProvider = (props) => {
   const initialToken = localStorage.getItem('token')
   const [token, setToken] = useState(initialToken);
 
   const userLoggedIn = !!token;
 
-  const loginHandler = (token) => {
-    setToken(token);
-    localStorage.setItem('token', token);
-  };
-
   const logoutHandler = () => {
     setToken(undefined);
     localStorage.removeItem('token')
   };
+
+  const loginHandler = (token, expirationTime) => {
+    setToken(token);
+    localStorage.setItem('token', token);
+    const remainingTime = calculateRemainingTime(expirationTime)
+    setTimeout(logoutHandler, remainingTime)
+  };
+
 
   const contextValue = {
     token: token,
