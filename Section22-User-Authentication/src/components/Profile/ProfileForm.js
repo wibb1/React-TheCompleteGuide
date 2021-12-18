95cb93b7a3c7b40e48fdc675a7b2ebe9a166dd9b
Will Campbell <wibb1@comcast.net>
2021-12-18 08:09:57 -0500

    added communication with firebase

--- Section22-User-Authentication/src/components/Profile/ProfileForm.js
+++ Section22-User-Authentication/src/components/Profile/ProfileForm.js
@@ -1,17 +1,54 @@
-import classes from './ProfileForm.module.css';
+import { useRef, useContext } from "react";
+import { useHistory } from "react-router-dom";
+
+import { API_KEY } from "../../secretKeys";
+import AuthContext from "../../store/auth-context";
+import classes from "./ProfileForm.module.css";
 
 const ProfileForm = () => {
+  const history = useHistory();
+  const authCtx = useContext(AuthContext);
+  const newPasswordInputRef = useRef();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const newPassword = newPasswordInputRef.current.value;
+    // I would normally add validation
+
+    fetch(
+      `https://identitytoolkit.googleapis.com/v1/accounts:update?key=${API_KEY}`,
+      {
+        method: "POST",
+        body: JSON.stringify({
+          idToken: authCtx.token,
+          password: newPassword,
+          returnSecureToken: false,
+        }),
+        headers: { "Content-Type": "application/json" },
+      }
+    ).then((res) => {
+      // currently assumes that we always succeed
+      history.replace('/');
+    });
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input
+          type="password"
+          id="new-password"
+          minLength="7"
+          ref={newPasswordInputRef}
+        />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
       </div>
     </form>
   );
-}
+};
 
 export default ProfileForm;
+
+//
